930ba181bfc3a45b7574fe0d3db4fe2cdea5d065
rli9 <rli9@.ccr.corp.intel.com>
2013-07-27 09:36:05 +0800

    Refactoring test code

--- rbprolog.gemspec
+++ rbprolog.gemspec
@@ -6,17 +6,19 @@ require 'rbprolog/version'
 Gem::Specification.new do |spec|
   spec.name          = "rbprolog"
   spec.version       = Rbprolog::VERSION
-  spec.authors       = ["ruijia.li"]
+  spec.authors       = ["Ruijia Li"]
   spec.email         = ["ruijia.li@gmail.com"]
   spec.description   = %q{A ruby implementation to simulate prolog partially}
   spec.summary       = %q{A prolog DSL in ruby to do AI logic analysis}
-  spec.homepage      = ""
+  spec.homepage      = "https://github.com/rli9/rbprolog"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
+
+  spec.metadata = { "Source Code" => "https://github.com/rli9/rbprolog" }
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
--- test/test_rbprolog.rb
+++ test/test_rbprolog.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require_relative '../lib/rbprolog'
+require 'rbprolog'
 
 class FriendLogic
   include Rbprolog
@@ -17,20 +17,43 @@ class FriendLogic
 end
 
 class TestRbprolog < Test::Unit::TestCase
-  def test_friend_logic
-    l = FriendLogic.new do
-      likes 'p5', 's1'
-    end
+  def test_fact_should_be_deduced
+    l = FriendLogic.new
 
     assert_equal true, l.likes?('p1', 's1')
     assert_equal true, l.likes?('p1', 's2')
+
+    assert_equal ['s1', 's2'], l.likes!('p1', Rbprolog::Var.new(:X)).map {|hash| hash[:X]}.uniq
+  end
+
+  def test_rule_should_be_deduced
+    l = FriendLogic.new
+
     assert_equal true, l.friends?('p1', 'p2')
     assert_equal true, l.friends?('p1', 'p3')
     assert_equal true, l.friends?('p1', 'p4')
     assert_equal false, l.friends?('p1', 'p6')
-    assert_equal true, l.likes?('p5', 's1')
 
-    assert_equal ['s1', 's2'], l.likes!('p1', Rbprolog::Var.new(:X)).map {|hash| hash[:X]}.uniq
-    assert_equal ['p1', 'p2', 'p3', 'p4', 'p5'], l.friends!('p1', Rbprolog::Var.new(:W)).map {|hash| hash[:W]}.uniq.sort
+    assert_equal ['p1', 'p2', 'p3', 'p4'], l.friends!('p1', Rbprolog::Var.new(:W)).map {|hash| hash[:W]}.uniq.sort
+  end
+
+  def test_logic_instances_should_be_independent
+    l1 = FriendLogic.new do
+      likes 'p5', 's1'
+    end
+
+    l2 = FriendLogic.new do
+      likes 'p5', 's2'
+    end
+
+    assert_equal true, l1.likes?('p5', 's1')
+    assert_equal false, l1.likes?('p5', 's2')
+    assert_equal true, l1.friends?('p5', 'p3')
+    assert_equal false, l1.friends?('p5', 'p2')
+
+    assert_equal false, l2.likes?('p5', 's1')
+    assert_equal true, l2.likes?('p5', 's2')
+    assert_equal false, l2.friends?('p5', 'p3')
+    assert_equal true, l2.friends?('p5', 'p2')
   end
 end
