9584fec097b330d39e13d46d02b07b34626ee10d
rli9 <ruijia.li@gmail.com>
2013-08-22 19:25:00 +0800

    update document for rbprolog.rb

--- lib/rbprolog.rb
+++ lib/rbprolog.rb
@@ -4,6 +4,51 @@ require 'rbprolog/rule'
 require 'rbprolog/deduction'
 require 'rbprolog/var'
 
+=begin rdoc
+Simulate the prolog logic processing partially by using ruby based DSL
+
+= Representations (Conventions)
+[rule - class level]
+  * name arg0, arg1, ..., {:if => [deduction0, deduction1, ...]}
+  * name: defined in the keywords
+  * arg: use const to present the variable, and non-const for value
+  * deduction: see below
+
+[fact - class level]
+  * name: arg0, arg1, ...
+
+[deduction - class level]
+  * name? arg0, arg1, ...
+  * return: true or false
+
+[question - instance level]
+  * name? arg0, arg1, ...
+
+[enumerator - instance level]
+  * name! arg0, arg1, ...
+  * Deduce all possible answers
+
+= Example
+  class FriendLogic
+    include Rbprolog
+
+    keywords :likes, :friends
+
+    likes 'p1', 's1'
+    likes 'p1', 's2'
+    likes 'p2', 's2'
+    likes 'p3', 's1'
+    likes 'p4', X
+
+    friends 'p1', W, :if => likes?(W, 's2')
+    friends X, Y, :if => [likes?(X, Z), likes?(Y, Z)]
+  end
+
+  l = FriendLogic.new
+  l.likes?('p1', 's1') #=> true
+  l.friends?('p1', 'p4') #=> true
+=end
+
 module Rbprolog
   def self.included(mod)
     class << mod
@@ -12,6 +57,18 @@ module Rbprolog
     end
   end
 
+  #
+  #Initialize the rbprolog instance, each instance can have its
+  #own fact and rules. The definition can be passed in as string or block.
+  #string is required when variable such as X is used.
+  #  l = FriendLogic.new do
+  #    likes 'p5', 's1'
+  #  end
+  #or
+  #  l = FriendLogic.new %q{
+  #    friends 'p2', X, :if => likes?(X, 's1')
+  #  }
+  #
   def initialize(string = nil, &block)
     if string || block
       self.extend(Rbprolog)
@@ -27,6 +84,7 @@ module Rbprolog
   end
 
   module ClassMethods
+    #Define the vocabulary of rules and facts
     def keywords(*syms)
       raise if syms.any? {|sym| sym.to_s.end_with? '?'}
 
@@ -38,6 +96,7 @@ module Rbprolog
       Var.new(sym)
     end
 
+    #Generate rule, fact and deduction based on conventions
     def method_missing(sym, *args)
       if self.syms.include? sym
         Hash === args.last ? rule(sym, *args) : rule(sym, *args, :if => [])
@@ -48,6 +107,7 @@ module Rbprolog
       end
     end
 
+    #Internal class method to install instance methods for question and enumerator
     def rule(sym, *args, options)
       self.rules ||= []
       self.rules << Rule.new(sym, *args, options[:if])
